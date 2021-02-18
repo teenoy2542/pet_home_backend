9d251fa477fe2a466a43f55f4f1d283235d1ed3c
inzee86333 <56289715+inzee86333@users.noreply.github.com>
2021-02-18 19:52:04 +0700

    x

--- animal_home_backend/settings.py
+++ animal_home_backend/settings.py
@@ -88,12 +88,8 @@ WSGI_APPLICATION = 'animal_home_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'animal_home',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
