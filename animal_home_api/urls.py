310be3627523e86174ad6b7a5db287e44ca44090
teenoy2542 <56290521+teenoy2542@users.noreply.github.com>
2021-02-24 06:13:49 +0700

    Add
    login : api/user/login

--- animal_home_api/urls.py
+++ animal_home_api/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
-from animal_home_api.views import PetView, PetDetailView, UserDetailView, RegisterAPI
 from django.conf import settings
 from django.conf.urls.static import static
+from knox import views as knox_views
 
-from rest_framework import routers
-from . import views
+from animal_home_api.views import PetView, PetDetailView, UserDetailView, RegisterAPI, LoginAPI
+
 
 urlpatterns = [
     path('api/user/register', RegisterAPI.as_view(), name='register'),
     path('api/user/<int:id>', UserDetailView.as_view()),
     path('api/pet', PetView.as_view()),
     path('api/pet/<int:pet_id>', PetDetailView.as_view()),
+    path('api/user/login', LoginAPI.as_view(), name='login')
 ]
 
 if settings.DEBUG:
--- animal_home_api/views.py
+++ animal_home_api/views.py
@@ -1,6 +1,12 @@
 from rest_framework import generics, permissions, serializers
-from knox.models import AuthToken
+from rest_framework import permissions
 from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+
+from knox.views import LoginView as KnoxLoginView
+from knox.models import AuthToken
+from django.contrib.auth import login
+
 
 from .models import PetUser, Pet, PetPhoto, Interested, Message
 from .serializers import UserSerializer, PetSerializer, PetPhotoSerializer, InterestedSerializer, MessageSerializer, RegisterSerializer
@@ -16,7 +22,18 @@ class RegisterAPI(generics.GenericAPIView):
         return Response({
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
-        }) 
+        })
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
 
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
